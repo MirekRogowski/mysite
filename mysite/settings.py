dbaa86b2a2222ab0ae433c6f819bc000777f01a7
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-02-07 20:59:44 +0100

    login logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,6 +124,8 @@ STATIC_URL = 'static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
