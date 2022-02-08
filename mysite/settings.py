10fe4ff666458fd7ad4802ab7784b01d490435ac
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-02-08 10:11:04 +0100

    create profile

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
