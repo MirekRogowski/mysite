8bd13e5496539636cdf72dc1be5d61f310f63275
MirekRogowski <93225975+MirekRogowski@users.noreply.github.com>
2022-02-11 15:35:54 +0100

    models

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
-from tinymce.models import HTMLField
+# from tinymce.models import HTMLField
 
 
 class Category(models.Model):
@@ -20,7 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     title_tag = models.CharField(max_length=200)
-    text =  HTMLField()
+    text = models.TextField()
     # created_date = models.DateTimeField(auto_now_add=True)
     created_date = models.DateTimeField(default=timezone.now)
     # published_date = models.DateTimeField(blank=True, null=True)
